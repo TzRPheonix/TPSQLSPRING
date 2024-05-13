22cc151790e75667fcbf881d4f25f9934c2ed6ec
TzRPheonix <tzrpheonix@gmail.com>
2024-05-13 09:57:47 +0200

    test AR

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { Entity, Scene } from 'aframe-react';
+import 'aframe';
+import 'aframe-ar';
 
 function App() {
+  useEffect(() => {
+    // Your GPS coordinates
+    const latitude = 50.64321886997472;
+    const longitude = 3.059177874980513;
+
+    // Set the GPS coordinates to the entity
+    const entity = document.querySelector('[gps-new-entity-place]');
+    entity.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Scene vr-mode-ui="enabled: false" arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false" renderer="antialias: true; alpha: true">
+        <Entity camera gps-new-camera="gpsMinDistance: 5"></Entity>
+        <Entity material="color: red" geometry="primitive: box" gps-new-entity-place="" scale="10 10 10"></Entity>
+      </Scene>
     </div>
   );
 }
